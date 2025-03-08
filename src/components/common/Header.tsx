2ced24e948a422416aa8af12e96d92fcf2f119c6
Eido <toandyjohnston@gmail.com>
2025-03-08 11:23:12 +0000

    Sat 8th

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,1 +1,22 @@
-export {};
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+import logo from '../../assets/images/logo.png';
+
+const Header: React.FC = () => {
+  return (
+    <header className="main-header">
+      <div className="logo">
+        <Link to="/">
+          <img src={logo} alt="LuvNav Logo" height="40" />
+          <span>LuvNav</span>
+        </Link>
+      </div>
+      <nav className="main-nav">
+        {/* You can add navigation items here later */}
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
